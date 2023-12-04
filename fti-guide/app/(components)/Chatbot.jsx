95218da125ea78284bc3779b38a9236c68ef4ae9
maite.l <maite.lejeune@student.howest.be>
2023-12-04 12:22:10 +0100

    fix localstorage attempt

--- fti-guide/app/(components)/Chatbot.jsx
+++ fti-guide/app/(components)/Chatbot.jsx
@@ -2,15 +2,19 @@
 
 import ChatBot from 'react-simple-chatbot';
 import { ThemeProvider } from 'styled-components';
+import { useEffect, useState } from 'react';
 
 
 const addToLocalStorage = (step, key, value) => {
-    const existingArray = JSON.parse(localStorage.getItem(key)) || [];
+    // make sure we are on client side
+    if (typeof window !== 'undefined') {
+        const existingArray = JSON.parse(localStorage.getItem(key)) || [];
 
-    // Check if the value is already in the array
-    if (!existingArray.includes(value)) {
-        const newArray = [...existingArray, value];
-        localStorage.setItem(key, JSON.stringify(newArray));
+        // check if the value is already in the array
+        if (!existingArray.includes(value)) {
+            const newArray = [...existingArray, value];
+            localStorage.setItem(key, JSON.stringify(newArray));
+        }
     }
 
     return step;
@@ -19,32 +23,48 @@ const addToLocalStorage = (step, key, value) => {
 
 export default function Chatbot({ steps, slug }) {
 
-    const locationsArray = JSON.parse(localStorage.getItem('location')) || [];
+    const [chatSteps, setChatSteps] = useState([{
+        "id": "placeholder",
+        "message": "Oh hey, jij weer!",
+        "end": true,
+        "hideInput": true
+    }]);
+    const [loading, setLoading] = useState(true);
 
-    if (!locationsArray.includes(slug)) {
-        steps = steps.filter(step => step.id !== "secondStart");
-    }
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
 
-    // parse the steps to add the trigger and component properties
-    steps = steps.map(step => {
-        const { trigger, component, ...rest } = step;
+            // change placeholder message out for fetched steps
+            const newSteps = steps;
+            setChatSteps(newSteps);
 
-        // if there is a component property, parse it as html
-        if (component) {
-            let content = step.component;
-            rest.component = (
-                <div dangerouslySetInnerHTML={{ __html: content }}></div>
-            );
+            // check if the location is already in the localstorage, if so, remove the secondStart step
+            const locationsArray = JSON.parse(localStorage.getItem('location')) || [];
+            if (!locationsArray.includes(slug)) {
+                const newSteps = steps.filter(step => step.id !== "secondStart");
+                setChatSteps(newSteps);
+            }
+
+            // go through all steps to parse the html and add functions
+            setChatSteps(prevSteps => prevSteps.map(step => {
+                const { trigger, component, ...rest } = step;
+                if (component) {
+                    let content = step.component;
+                    rest.component = (
+                        <div dangerouslySetInnerHTML={{ __html: content }}></div>
+                    );
+                }
+                if (trigger && trigger.function === "addToLocalStorage") {
+                    rest.trigger = () => addToLocalStorage(trigger.arguments.trigger, trigger.arguments.key, trigger.arguments.value);
+                } else if (trigger) {
+                    rest.trigger = trigger;
+                }
+                return rest;
+            }));
+
+            setLoading(false);
         }
-        // if the trigger is a function, add function in trigger
-        if (trigger && trigger.function === "addToLocalStorage") {
-            rest.trigger = () => addToLocalStorage(trigger.arguments.trigger, trigger.arguments.key, trigger.arguments.value);
-        } else if (trigger) {
-            rest.trigger = trigger;
-        }
-
-        return rest;
-    });
+    }, [slug]);
 
     const theme = {
         background: '#000000',
@@ -60,41 +80,43 @@ export default function Chatbot({ steps, slug }) {
 
     return (
         <ThemeProvider theme={theme}>
-            <ChatBot
-                hideHeader="true"
-                hideBotAvatar="true"
-                hideUserAvatar="true"
-                hideSubmitButton="true"
-                footerStyle={{ display: 'none' }}
-                contentStyle={{
-                    height: '90vh',
-                    paddingBlockEnd: '20px',
-                }}
-                enableSmoothScroll="true"
-                style={{ borderRadius: '0px', boxShadow: 'none' }}
-                bubbleOptionStyle={{ border: '2px #EF6C00 solid', backgroundColor: 'transparent', color: '#EF6C00', marginBlockStart: '4px', marginInlineStart: '4px', textAlign: 'left' }}
-                customStyle={{
-                    // backgroundColor: 'transparent',
-                    display: 'flex',
-                    justifyContent: 'flex-start',
-                    padding: '12px',
-                    margin: '0px',
-                    boxShadow: 'none',
-                    backgroundColor: '#EF6C00',
-                    //   display: 'flex',
-                    //   flexDirection: 'column',
-                    color: '#fff',
-                    fontFamily: 'Helvetica Neue',
-                    fontSize: '14px',
-                    borderRadius: '18px 18px 18px 0px',
-                    width: "max-content",
-                    padding: '12px',
-                    marginLeft: '12px',
-                    marginBlockStart: '-4px',
-                    marginBlockEnd: '10px',
-                }}
-                steps={steps}
-            />
+            {!loading && (
+                <ChatBot
+                    hideHeader="true"
+                    hideBotAvatar="true"
+                    hideUserAvatar="true"
+                    hideSubmitButton="true"
+                    footerStyle={{ display: 'none' }}
+                    contentStyle={{
+                        height: '90vh',
+                        paddingBlockEnd: '20px',
+                    }}
+                    enableSmoothScroll="true"
+                    style={{ borderRadius: '0px', boxShadow: 'none' }}
+                    bubbleOptionStyle={{ border: '2px #EF6C00 solid', backgroundColor: 'transparent', color: '#EF6C00', marginBlockStart: '4px', marginInlineStart: '4px', textAlign: 'left' }}
+                    customStyle={{
+                        // backgroundColor: 'transparent',
+                        display: 'flex',
+                        justifyContent: 'flex-start',
+                        padding: '12px',
+                        margin: '0px',
+                        boxShadow: 'none',
+                        backgroundColor: '#EF6C00',
+                        //   display: 'flex',
+                        //   flexDirection: 'column',
+                        color: '#fff',
+                        fontFamily: 'Helvetica Neue',
+                        fontSize: '14px',
+                        borderRadius: '18px 18px 18px 0px',
+                        width: "max-content",
+                        padding: '12px',
+                        marginLeft: '12px',
+                        marginBlockStart: '-4px',
+                        marginBlockEnd: '10px',
+                    }}
+                    steps={chatSteps}
+                />
+            )}
         </ThemeProvider>
     )
 }
