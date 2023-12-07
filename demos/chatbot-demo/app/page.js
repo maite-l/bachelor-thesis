5f32fe87aacb6e0f34dc8a6433f7b6189bda4aaf
maite.l <maite.lejeune@student.howest.be>
2023-12-07 14:03:49 +0100

    adjusted chatbot demo content to show during testing moment

--- demos/chatbot-demo/app/page.js
+++ demos/chatbot-demo/app/page.js
@@ -11,17 +11,17 @@ export function CustomOptions({ options, triggerNextStep }) {
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', gap: '0.5rem'}}>
+    <div style={{ display: 'flex', flexDirection: 'row', gap: '0.5rem' }}>
       {options.map((option, index) => (
         <div
           key={index}
           onClick={() => handleOptionClick(option)}
-          style={{ 
+          style={{
             border: option.primary ? '2px #EF6C00 solid' : 'none',
             textDecoration: option.primary ? 'none' : 'underline',
-            color: 'white', 
-            padding: '10px', 
-            borderRadius: '20px' 
+            color: 'white',
+            padding: '10px',
+            borderRadius: '20px'
           }}
         >
           {option.value}
@@ -50,16 +50,16 @@ export default function Home() {
     // Execute your function based on the step ID or any other condition
     console.log(`Step changed to ${step}`);
 
-    const existingArray = JSON.parse(localStorage.getItem('yourKey')) || [];
-    const newArray = [...existingArray, 'newElement'];
-    localStorage.setItem('yourKey', JSON.stringify(newArray));
+    const existingArray = JSON.parse(localStorage.getItem('key')) || [];
+    const newArray = [...existingArray, 'this was added by the chatbot'];
+    localStorage.setItem('key', JSON.stringify(newArray));
 
     return step;
   };
 
   const insertedStep = {
     id: '6',
-    message: 'What are you looking for?',
+    message: 'This is a message that was dynamically inserted in the middle of the chatbot',
     // trigger: '7',
     end: true,
     hideInput: true
@@ -100,58 +100,60 @@ export default function Home() {
 
           // will have to be the same styling as bubbleStyle with the colours of the bot in the theme
           customStyle={{
-          //   backgroundColor: '#EF6C00',
-          //   display: 'flex',
-          //   flexDirection: 'column',
-          //   color: '#fff',
-          //   fontFamily: 'Helvetica Neue',
-          //   fontSize: '15px',
-          //   borderRadius: '15px 15px 15px 0px',
-          //   width: "max-content",
-          backgroundColor: 'transparent',
-          display: 'flex',
-          justifyContent: 'flex-start',
-          padding: '12px',
-          margin: '0px'
+            //   backgroundColor: '#EF6C00',
+            //   display: 'flex',
+            //   flexDirection: 'column',
+            //   color: '#fff',
+            //   fontFamily: 'Helvetica Neue',
+            //   fontSize: '15px',
+            //   borderRadius: '15px 15px 15px 0px',
+            //   width: "max-content",
+            backgroundColor: 'transparent',
+            display: 'flex',
+            justifyContent: 'flex-start',
+            padding: '12px',
+            margin: '0px'
           }}
 
           steps={[
             {
               id: '1',
-              message: 'What are you looking for?',
+              message: 'Hello, this is a message you can reply to with options.',
               trigger: '2',
               hideInput: true
             },
             {
               id: '2',
               options: [
-                { value: '1', label: 'games', trigger: 4 },
-                { value: '2', label: 'edtech', trigger: 4 },
+                { value: '1', label: 'this is a standard option', trigger: 3 },
+                { value: '2', label: 'this is another standard option', trigger: 3 },
               ],
             },
             {
               id: '3',
-              component: <CustomOptions options={[{ value: 'games', trigger: 4, primary: true }, { value: 'edtech', trigger: 5 }]} />,
-              replace: true,
+              message: 'This is a message that put something in localstorage',
+              trigger: () => handleStepChange(4),
+              hideInput: true,
             },
             {
               id: '4',
-              message: 'Wrong answer, try again.',
-              trigger: () => handleStepChange(3),
-              hideInput: true,
+              component: <CustomOptions options={[{ value: 'This is a custom option component', trigger: 5, primary: true }, { value: 'We created this option ourselves', trigger: 5 }]} />,
+              replace: true,
             },
             {
               id: '5',
               // asMessage: true,
               component: (
-                <div>
+                <div
+                  style={{ border: '0', backgroundColor: '#EF6C00', padding: '0.5rem', borderRadius: '0.5rem' }}
+                >
                   <p
                     style={{ marginBottom: '0.5rem' }}
-                  >I think you will like this location!</p>
+                  >We made this component to test if we can embed Google Maps</p>
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.9792070644253!2d3.260466376448733!3d50.831549059851184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c33afcb2d35a69%3A0xb3e9316f20e0416a!2sBUDAfabriek!5e0!3m2!1sen!2sbe!4v1700641989699!5m2!1sen!2sbe"
-                    width="300"
-                    height="300"
+                    width="250"
+                    height="250"
                     style={{ border: '0' }}
                     allowFullScreen=""
                     loading="lazy"
@@ -159,7 +161,6 @@ export default function Home() {
                   ></iframe>
                 </div>
               ),
-              // end: true,
               hideInput: true,
               trigger: '6'
             },
