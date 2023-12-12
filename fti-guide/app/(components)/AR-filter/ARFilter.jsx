41e6d446a65a1fd2c10516f580ee005b7bfa725d
maite.l <maite.lejeune@student.howest.be>
2023-12-12 14:21:54 +0100

    test ar filter improvements 15

--- fti-guide/app/(components)/AR-filter/ARFilter.jsx
+++ fti-guide/app/(components)/AR-filter/ARFilter.jsx
@@ -44,7 +44,7 @@ export default function ARFilter() {
         const canvasV = await html2canvas(video);
         const dataV = canvasV.toDataURL('image/jpg');
 
-        // console.log(dataV);
+        console.log(dataV);
 
         // dom-to-image to get the filter as an image (html2canvas doesn't support transparency in canvas element)
         const dataF = await domtoimage.toPng(filter);
