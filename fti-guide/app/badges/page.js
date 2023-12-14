4f5507f78b696d8ad37acf720a1f940af6849864
maite.l <maite.lejeune@student.howest.be>
2023-12-14 16:10:34 +0100

    test ios problem 7

--- fti-guide/app/badges/page.js
+++ fti-guide/app/badges/page.js
@@ -11,7 +11,7 @@ export default function BadgesPage() {
 
   return (
     <main>
-      {/* <BadgeCollection allBadges={allBadges} /> */}
+      <BadgeCollection allBadges={allBadges} />
 
       <svg xmlns="http://www.w3.org/2000/svg" width="8" height="43" viewBox="0 0 8 43" fill="none" className={styles.arrow}>
         <path d="M3.64645 42.8536C3.84171 43.0488 4.15829 43.0488 4.35355 42.8536L7.53553 39.6716C7.7308 39.4763 7.7308 39.1597 7.53553 38.9645C7.34027 38.7692 7.02369 38.7692 6.82843 38.9645L4 41.7929L1.17157 38.9645C0.976311 38.7692 0.659728 38.7692 0.464466 38.9645C0.269204 39.1597 0.269204 39.4763 0.464466 39.6716L3.64645 42.8536ZM3.5 0V42.5H4.5V0H3.5Z" fill="#6E33D5" />
