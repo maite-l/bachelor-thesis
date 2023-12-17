a9bc00c987400529eaefac807921cf343b0b8e2b
maite.l <maite.lejeune@student.howest.be>
2023-12-17 18:41:37 +0100

    attempt at adding home screen icons

--- fti-guide/app/layout.js
+++ fti-guide/app/layout.js
@@ -28,7 +28,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-
+        <link rel="apple-touch-icon" sizes="57x57" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/apple-icon-57x57.png"></link>
+        <link rel="apple-touch-icon" sizes="60x60" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/apple-icon-60x60.png"></link>
+        <link rel="apple-touch-icon" sizes="72x72" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/apple-icon-72x72.png"></link>
+        <link rel="apple-touch-icon" sizes="76x76" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/apple-icon-76x76.png"></link>
+        <link rel="apple-touch-icon" sizes="114x114" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/apple-icon-114x114.png"></link>
+        <link rel="apple-touch-icon" sizes="120x120" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/apple-icon-120x120.png"></link>
+        <link rel="apple-touch-icon" sizes="144x144" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/apple-icon-144x144.png"></link>
+        <link rel="apple-touch-icon" sizes="152x152" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/apple-icon-152x152.png"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/apple-icon-180x180.png"></link>
+        <link rel="icon" type="image/png" sizes="192x192" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/android-icon-192x192.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="96x96" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/favicon-96x96.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/favicon-16x16.png"></link>
+        <link rel="manifest" href="2fe7b9dfb30166a88281cf320b1b3c46.ico/manifest.json"></link>
+        <meta name="msapplication-TileColor" content="#ffffff"></meta>
+        <meta name="msapplication-TileImage" content="2fe7b9dfb30166a88281cf320b1b3c46.ico/ms-icon-144x144.png"></meta>
+        <meta name="theme-color" content="#ffffff"></meta>
         <title>RoBob - Jouw gids op FTI Kortrijk</title>
       </head>
       <body className={`${Lausanne.className} ${GTSuper.variable}`}>
