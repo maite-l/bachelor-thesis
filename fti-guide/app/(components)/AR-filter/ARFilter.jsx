e27407db8215fd0bada8d12a332a0ebc77a6912c
maite.l <maite.lejeune@student.howest.be>
2023-12-18 15:09:40 +0100

    filter size test 2

--- fti-guide/app/(components)/AR-filter/ARFilter.jsx
+++ fti-guide/app/(components)/AR-filter/ARFilter.jsx
@@ -128,7 +128,7 @@ export default function ARFilter({ allBadges }) {
         arFilterRef.current.startTracking();
     };
 
-    let scale = 1;
+    let scale = 1.5;
     let offset = 0;
 
     return (
